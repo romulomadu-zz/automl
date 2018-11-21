a1aceb3421fdfaaca9376a542dae93a49af4baa8
romulomadu <romulomadu@gmail.com>
2018-11-21 20:22:53 -0200

    change nmse formula

--- config/params.py
+++ config/params.py
@@ -1,16 +1,14 @@
 import numpy as np
 
-from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.metrics import make_scorer, mean_squared_error, r2_score
 from scipy.stats import uniform as sp_uniform
 from skopt.space import Real
 
 
 def nmse(y_pred, y_true, verbose=0):
 	"""	Normalized mean squared error."""
-	y_mean = np.ones(y_true.shape[0]) * np.ndarray.mean(y_true)
-	epsilon = 1e-15
 
-	error = mean_squared_error(y_true, y_pred) / (mean_squared_error(y_true, y_mean) + epsilon)
+	error = 1 - r2_score(y_true, y_pred)
 	if verbose:
 		print('NMSE: {:}'.format(error))
 	return error
@@ -20,7 +18,7 @@ def grid_params():
 	"""Grid Search parameters."""
 	gamma = [2 ** i for i in range(-15, 4, 1)]
 	C = [2 ** i for i in range(-5, 16, 1)]
-	epsilon = [2 ** i for i in range(-8, 2, 1)]
+	epsilon = [2 ** i for i in range(-8, 0, 1)]
 
 	param_grid = {
 		'param_grid': {'kernel': ['rbf'], 'C': C, 'gamma': gamma, 'epsilon': epsilon},
@@ -36,7 +34,7 @@ def random_params():
 	"""Random Search parameters."""
 	gamma = sp_uniform(2 ** -15, 2 ** 3)
 	C = sp_uniform(2 ** -5, 2 ** 15)
-	epsilon = sp_uniform(2 ** -8, 2 ** 1)
+	epsilon = sp_uniform(2 ** -8, 2 ** -1)
 
 	param_dist = {
 		'param_distributions': {'kernel': ['rbf'], 'C': C, 'gamma': gamma, 'epsilon': epsilon},
@@ -53,7 +51,7 @@ def bayes_params():
 	"""Random Search parameters."""
 	gamma = Real(2 ** -15, 2 ** 3, prior='log-uniform')
 	C = Real(2 ** -5, 2 ** 15, prior='log-uniform')
-	epsilon = Real(2 ** -8, 2 ** 1, prior='log-uniform')
+	epsilon = Real(2 ** -8, 2 ** -1, prior='log-uniform')
 
 	search_spaces = {
 		'search_spaces': {'kernel': ['rbf'], 'C': C, 'gamma': gamma, 'epsilon': epsilon},
